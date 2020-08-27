07ea07d5db7f86f323b1132284266e50a0c8b4a4
Maksim Pavlov <pavlovmark100@gmail.com>
2020-08-27 13:22:52 +0400

    fix: fix styles main tab

--- src/components/TabView/TabView.tsx
+++ src/components/TabView/TabView.tsx
@@ -6,6 +6,8 @@ import axios from "axios";
 import { urlApi } from "../../data/const";
 import { EventData } from "../types";
 
+import './tab-view.scss'
+
 const TabView: React.FC = () => {
     const { TabPane } = Tabs;
     const [allEventsData, setAllEventsData] = useState<EventData[]>([]);
@@ -19,17 +21,19 @@ const TabView: React.FC = () => {
         })
     },[]);
     return (
-        <Tabs type="card">
-            <TabPane tab="TABLE" key="1">
-                <MainTable loaderState={loaderState} allEventsData={allEventsData}  />
-            </TabPane>
-            <TabPane tab="CALENDAR" key="2">
-                <CalendarView allEventsData={allEventsData} />
-            </TabPane>
-            <TabPane tab="LIST" key="3">
-                Content of Tab Pane 3
-            </TabPane>
-        </Tabs>
+        <section className='main_tab_section'>
+            <Tabs type="card">
+                <TabPane tab="TABLE" key="1">
+                    <MainTable loaderState={loaderState} allEventsData={allEventsData}  />
+                </TabPane>
+                <TabPane tab="CALENDAR" key="2">
+                    <CalendarView allEventsData={allEventsData} />
+                </TabPane>
+                <TabPane tab="LIST" key="3">
+                    Content of Tab Pane 3
+                </TabPane>
+            </Tabs>
+       </section>
     )
 }
 
