55201b81d4faf15df02fad9c3cef74102426898e
Iuliia Plekhanova <julec2006@yandex.ru>
2020-08-29 18:28:42 +0400

    feat: add list view

---
+++ src/components/ListView/ListView.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Timeline } from 'antd';
+
+import { SystemState } from "../../redux/types";
+import { EventData } from "../types";
+
+import './ListView.scss'
+
+
+interface Props {
+  allEventsData: EventData[];
+}
+
+const ListView: React.FC<Props> = ({ allEventsData }) => {
+  const events = allEventsData.map((event) => {
+    return (
+      <Timeline.Item
+        color={ event.type === "Deadline" ? "red" : "green" }
+        key={event.id}>
+          <a href={event.optional.description}>
+            { event.optional.date.slice(0, -9) + " " + event.name }
+          </a>
+      </Timeline.Item>
+    )
+  })
+
+  return (
+    <Timeline>
+      {events}
+    </Timeline>
+ )
+}
+
+
+const mapStateToProps = (state: SystemState) => ({
+  allEventsData: state.allEventsData,
+})
+
+export default connect(mapStateToProps)(ListView);
