710b890d5fcbced9adcdd6b7d9351b7860f3c0e5
Maksim Pavlov <pavlovmark100@gmail.com>
2020-08-30 18:03:32 +0400

    feat: add csv format

--- src/components/TopPanel/TopPanel.tsx
+++ src/components/TopPanel/TopPanel.tsx
@@ -2,24 +2,27 @@ import React, { useEffect, useState } from "react";
 import { Button, Avatar, Switch, Typography } from 'antd';
 import { DownloadOutlined, SettingOutlined } from '@ant-design/icons';
 import { connect , useSelector } from 'react-redux';
+import { CSVLink, CSVDownload } from "react-csv";
 
 import './TopPanel.scss';
 
 import { SystemState } from "../../redux/types";
 import { EventData } from "../types";
+import {getEventsDataForCsv} from "./helpers/getEventsDataForCsv";
 
 interface Props {
     allEventsData: EventData[];
 }
 
 
-const TopPanel: React.FC<Props> = () => {
+const TopPanel: React.FC<Props> = ({allEventsData}) => {
+    const csvData = getEventsDataForCsv(allEventsData);
   return (
     <div className="top-panel">
       <div className="save-container">
           <p>Save schedule as:
-              <a>xlsx</a>,
-              <a>pdf</a>
+              <a> xlsx</a>,
+              <CSVLink data={csvData} separator={"  "}> .csv</CSVLink>
               <DownloadOutlined /> </p>
       </div>
       <Button className="settings-btn">Settings <SettingOutlined /> </Button>
