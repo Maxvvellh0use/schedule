c0d6f4e0311575b8173eca370391f84fe46a93a4
Maksim Pavlov <pavlovmark100@gmail.com>
2020-08-30 16:34:30 +0400

    feat: add check receiving events data from API

--- src/components/MainTab/MainTab.tsx
+++ src/components/MainTab/MainTab.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Tabs } from 'antd';
 import { useSelector } from "react-redux";
+import { connect } from 'react-redux';
 
 import './MainTab.scss'
 
 import CalendarView from "../CalendarView/CalendarView";
 import TableView from "../TableView/TableView";
 import ListView from "../ListView/ListView";
+import {SystemState} from "../../redux/types";
+import {EventData} from "../types";
 
 interface RootState {
     app: {
@@ -14,23 +17,28 @@ interface RootState {
     },
 }
 
+interface Props {
+    allEventsData: EventData[],
+}
 
-const MainTab: React.FC = () => {
+
+const MainTab: React.FC<Props> = ({ allEventsData }) => {
     const { TabPane } = Tabs;
     const errorText = useSelector<RootState, string>(state => state.app.errorText);
+    const isEventsDataReceived = () => Boolean(!allEventsData.length);
     return (
         <section className='main_tab_section'>
             <Tabs type="card">
                 <TabPane tab="TABLE" key="1">
                     <TableView />
                 </TabPane>
-                <TabPane tab="CALENDAR" key="2">
+                <TabPane disabled={isEventsDataReceived()} tab="CALENDAR" key="2">
                     {
                         errorText ? <div>{errorText}</div> :
                     <CalendarView />
                     }
                 </TabPane>
-                <TabPane tab="LIST" key="3">
+                <TabPane disabled={isEventsDataReceived()} tab="LIST" key="3">
                     {
                         errorText ? <div>{errorText}</div> :
                         <ListView/>
@@ -41,4 +49,8 @@ const MainTab: React.FC = () => {
     )
 }
 
-export default MainTab;
+const mapStateToProps = (state: SystemState) => ({
+    allEventsData: state.allEventsData,
+})
+
+export default connect(mapStateToProps)(MainTab);
