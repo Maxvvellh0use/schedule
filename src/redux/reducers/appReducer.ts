6e108db9e77a75a6618932a0a6dec15c8f7bf5e1
Eduard Brukish <eduardbrukish@gmail.com>
2020-09-10 21:49:35 +0300

    feat: add language to global state

--- src/redux/reducers/appReducer.ts
+++ src/redux/reducers/appReducer.ts
@@ -1,10 +1,11 @@
-import { CHANGE_MODE , HIDE_LOADER , SHOW_ERROR , SHOW_LOADER, CHANGE_ACCESSABILITY } from "../types";
+import { CHANGE_MODE , HIDE_LOADER , SHOW_ERROR , SHOW_LOADER, CHANGE_ACCESSABILITY, SET_ENGLISH, SET_RUSSIAN } from "../types";
 
 const initialState = {
     loading: false,
     errorText: '',
     mode: localStorage.mode ? localStorage.mode : 'student',
-    accessability: localStorage.accessability ? localStorage.accessability === 'true' : false
+    accessability: localStorage.accessability ? localStorage.accessability === 'true' : false,
+    language: localStorage.language ? localStorage.language : 'eng'
 }
 
 export const appReducer = (state = initialState, action: { type: string, payload: boolean | string }) => {
@@ -19,6 +20,10 @@ export const appReducer = (state = initialState, action: { type: string, payload
             return Object.assign({...state},{ mode: action.payload })
         case CHANGE_ACCESSABILITY:
             return Object.assign({...state},{ accessability: action.payload })
+        case SET_ENGLISH:
+            return Object.assign({...state},{ language: 'eng' })
+        case SET_RUSSIAN:
+            return Object.assign({...state},{ language: 'ru' })
         default: return state;
     }
 }
