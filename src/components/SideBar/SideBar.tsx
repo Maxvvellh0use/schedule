407d763004951bb5ac70461a22ee47bec7453712
Maksim Pavlov <pavlovmark100@gmail.com>
2020-09-18 20:20:00 +0400

    fix: remove border from Plan for today

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from "react";
 import { Calendar, Typography, List, Divider } from 'antd';
 import { ThunderboltOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
+import { setDate } from "../../redux/actions";
 import { Link } from "react-router-dom";
 
 import './SideBar.scss'
 import { EventData, RootStateType } from "../types";
 import { getTodayEvents } from "./helpers";
-import { setDate } from "../../redux/actions";
 import moment from "moment";
+
 
 const SideBar: React.FC = () => {
   const allEventsData = useSelector<RootStateType, EventData[]>(state => state.allEventsData);
   const { Title, Text } = Typography;
   const todayEvents = allEventsData.length ? getTodayEvents(allEventsData) : undefined;
   const dispatch = useDispatch();
- 
+
+
   function onSelect(value: moment.Moment) {
-    dispatch(setDate(value.format('DD.MM.YYYY')))
+        dispatch(setDate(value.format('DD.MM.YYYY')))
   }
 
   const language = useSelector<RootStateType, string>(state => state.app.language);
@@ -35,7 +37,6 @@ const SideBar: React.FC = () => {
               <ThunderboltOutlined className="announce-icon" style={{ color: 'green'}} />
               <Title level={4}>{discAnnounce}</Title>
             </div>}
-          bordered
           dataSource={todayEvents}
           renderItem={(event: EventData) =>
             <List.Item>
